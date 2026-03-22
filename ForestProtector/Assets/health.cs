da27524c09d8c83977b2118281f5882a8cf0514b
yassinghalit65-netizen <yassin.ghalit@edu-kw1c.nl>
2026-03-22 15:20:12 +0100

    health
    de health gedeeltelijk opgelost

--- ForestProtector/Assets/health.cs
+++ ForestProtector/Assets/health.cs
@@ -1,43 +1,68 @@
 using UnityEngine;
-
-
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 	public float maxHealth = 100f;
 	float currentHealth;
 
-
-
 	public GameObject house1;
 	public GameObject house2;
 	public GameObject house3;
 
-
+	public Slider healthBar;
 
 	void Start()
 	{
 		currentHealth = maxHealth;
 
+		// Zorg dat je healthbar goed werkt
+		if (healthBar != null)
+		{
+			healthBar.maxValue = maxHealth;
+			healthBar.value = currentHealth;
+		}
 
-
-		house1.SetActive(true);
-		house2.SetActive(false);
-		house3.SetActive(false);
-
-
+		UpdateHouse();
 	}
 
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			TakeDamage(1f);
+		}
 
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			RebuildHouse(1f);
+		}
+	}
 
 	public void TakeDamage(float damage)
 	{
-
-
 		currentHealth -= damage;
+		currentHealth = Mathf.Max(currentHealth, 0);
 
+		UpdateHouse();
+		UpdateHealthBar();
 
+		Debug.Log("Health: " + currentHealth);
+	}
 
+	public void RebuildHouse(float amount)
+	{
+		currentHealth += amount;
+		currentHealth = Mathf.Min(currentHealth, maxHealth);
+
+		UpdateHouse();
+		UpdateHealthBar();
+
+		Debug.Log("Health: " + currentHealth);
+	}
+
+	void UpdateHouse()
+	{
 		if (currentHealth <= 20)
 		{
 			house1.SetActive(false);
@@ -50,29 +75,7 @@ public class Health : MonoBehaviour
 			house2.SetActive(true);
 			house3.SetActive(false);
 		}
-
-
-
-	}
-	public void RebuildHouse(float damage)
-	{
-		currentHealth += damage;
-
-
-
-		if (currentHealth >= 20)
-		{
-			house1.SetActive(false);
-			house2.SetActive(false);
-			house3.SetActive(true);
-		}
-		else if (currentHealth >= 50)
-		{
-			house1.SetActive(false);
-			house2.SetActive(true);
-			house3.SetActive(false);
-		}
-		else if (currentHealth >= 100)
+		else
 		{
 			house1.SetActive(true);
 			house2.SetActive(false);
@@ -80,6 +83,11 @@ public class Health : MonoBehaviour
 		}
 	}
 
-
-
+	void UpdateHealthBar()
+	{
+		if (healthBar != null)
+		{
+			healthBar.value = currentHealth;
+		}
+	}
 }
